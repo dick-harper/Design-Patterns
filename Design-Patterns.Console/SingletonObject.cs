8fc0950b998b0a03eb75c87b2fd80e190c16b6c8
Richard Harper <dick.harper@gmail.com>
2017-07-28 17:12:16 -0600

    Work on singleton pattern.

---
+++ Design-Patterns.Console/SingletonObject.cs
@@ -0,0 +1,23 @@
+﻿namespace Design_Patterns.Console
+{
+    public class SingletonObject
+    {
+        private static SingletonObject instance;
+
+        private SingletonObject() { }
+
+        public static SingletonObject Instance
+        {
+            get
+            {
+                // lazy load feature
+                if (instance == null)
+                {
+                    instance = new SingletonObject();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
